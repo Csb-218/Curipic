5a5ab096de6618e3bb97f49de7ae0c3477992315
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2025-01-25 00:31:45 +0530

    Update photosController.js

--- controllers/photosController.js
+++ controllers/photosController.js
@@ -1,15 +1,20 @@
 const db = require('../models');
-const { sequelize } = db;
+const { sequelize } = db
+const { Op } = require('sequelize');
 
 const axiosInstance = require('../lib/axiosInstance');
 
 const {
     photo: photosModel,
-    tags: tagsModel,
+    tag: tagsModel,
     user: userModel,
+    phototags : photoTagModel,
+    searchHistory: searchHistoryModel
 } = sequelize.models
 
-console.log(sequelize.models)
+
+console.log(sequelize.models,11)
+
 const searchPhotos = async (req, res) => {
 
     const { query } = req.query;
@@ -66,6 +71,8 @@ const savePhoto = async (req, res) => {
 
     try {
 
+
+
         const isUserIdPresent = await userModel.findOne({ where: { id: userId } })
 
         if(!isUserIdPresent) return res.status(400).json({"error message": "user id not found"})
@@ -82,13 +89,27 @@ const savePhoto = async (req, res) => {
                     }
                 })
 
-                if(!isTag) return res.status(400).json({"error message" : "invalid tags "})
+                if(!isTag){
+                    const newTag = await tagsModel.create({ name: tag })
+                    
+                    photoTagModel.create({
+                        tag_id:newTag.id,
+                        photo_id:newPhoto.id
+                    })
 
-                isTag.photoId = newPhoto.id ;
 
-                await isTag.save();
+                }else{
 
+
+                    photoTagModel.create({
+                        tag_id:isTag.id,
+                        photo_id:newPhoto.id
+                    })
+
+                }
+                    // return res.status(400).json({"error message" : "invalid tags "})
             }
+            
 
         }
 
@@ -98,7 +119,7 @@ const savePhoto = async (req, res) => {
     } catch (error) {
         console.error(error)
 
-        return res.status(500).json({"error message" : "Internal server error"})
+        return res.status(500).json({"error message" : "Internal Server Error"})
 
     }
 
@@ -107,7 +128,136 @@ const savePhoto = async (req, res) => {
 
 }
 
+const changePhotoTags = async (req, res) => {
+
+
+    const {  tags } = req.body;
+    const { photoId } = req.params;
+
+    if(!photoId) return res.status(400).json({"error message" : " 'photoId' is required "})
+    if(!tags) return res.status(400).json({"error message" : " 'tags' required "})
+    if(tags.length <= 0) return res.status(400).json({"error message" : " at least one tag is required "})
+
+   
+    try {
+
+       const photo = await photosModel.findByPk(parseInt(photoId))
+
+        if (photo) {
+
+            for (let tag of tags) {
+
+                const isTag = await tagsModel.findOne({
+                    where: {
+                        name : tag
+                    }
+                })
+
+                if(!isTag){
+
+                    const newTag = await tagsModel.create({ name: tag })
+
+                    photoTagModel.create({
+                        tag_id:newTag.id,
+                        photo_id:photo.id
+                    })
+
+                }else{
+
+                    photoTagModel.create({
+                        tag_id:isTag.id,
+                        photo_id:photo.id
+                    })
+
+                } // return res.status(400).json({"error message" : "invalid tags "})
+            }
+            
+            return res.status(201).json({'message': 'Photo saved successfully'})
+        }
+
+        return res.status(400).json({'error message': 'Photo not found with id ' + photoId})
+
+
+    } catch (error) {
+        console.error(error)
+
+        return res.status(500).json({"error message" : "Internal Server Error"})
+
+    }
+}
+
+const searchByTag = async(req,res) =>{
+
+    const {tag,sort,userId} = req.query
+
+    console.log(sort)
+
+
+    if(!tag) return res.status(400).json({"error message" : "tag not provided"})
+    
+    if(!parseInt(userId)) return res.status(400).json({"error message" : "userId not provided"})
+
+    if(sort !== "ASC" && sort !== "DESC") return res.status(400).json({"error message" : "Incorrect sort order"})
+
+    // search history
+
+    await searchHistoryModel.create({
+        userId:userId,
+        query : tag
+    })
+
+
+    try{
+
+        const _tag = await tagsModel.findOne({
+            where : {
+                name : tag
+            }
+        })
+
+        if(!tag || tag.length <=0) return res.status(400).json({"error message" : "tag not found"})
+
+        const phototags = await photoTagModel.findAll({
+            where:{
+                tag_id : _tag.dataValues.id
+            }
+        })
+
+        if(!phototags || phototags.length <=0) return res.status(400).json({"error message" :  "tag not found"})
+
+        const photos = await photosModel.findAll({
+            where:{
+                id: {
+                    [Op.in]: phototags.map(item=>item.dataValues.photo_id)
+                },
+                userId : userId
+            },
+            order: [
+                ['createdAt', sort]
+            ]
+        })
+
+        if(photos.length <= 0) return res.status(404).json({"error message" : "No photos found "})
+
+        
+
+        
+        
+
+        return res.status(200).json({"photos":photos})
+       
+    }catch(err){
+        console.error(err)
+        return res.status(500).json({"error message" : "Internal Server Error"})
+    }
+    
+
+
+}
+
 module.exports = {
     searchPhotos,
-    savePhoto
+    savePhoto,
+    changePhotoTags,
+    searchByTag
 }
