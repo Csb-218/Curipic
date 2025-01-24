c79789192603d4e5fdba787c1af182fc8e847a2f
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2025-01-25 00:33:19 +0530

    Update photos.js

--- routes/photos.js
+++ routes/photos.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-const {searchPhotos,savePhoto} = require("../controllers/photosController")
+const {searchPhotos,savePhoto,changePhotoTags,searchByTag} = require("../controllers/photosController")
 
 router.post('/',savePhoto);
 
 router.get('/search', searchPhotos)
 
-router.post('/:photoId/tags', searchPhotos)
+router.post('/:photoId/tags', changePhotoTags)
+
+router.get('/tag/search',searchByTag)
 
 module.exports = router;
