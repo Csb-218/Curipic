d6044e69d0c6ceeabcf666d0b6556204c47897fc
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2025-01-25 00:33:08 +0530

    Update searchhistory.js

--- models/searchhistory.js
+++ models/searchhistory.js
@@ -1,16 +1,27 @@
-const { DataTypes } = require('sequelize');
-const db = require('../config/config.js');
-
-const searchHistory= db.sequelize.define('searchHistory', {
-  query: DataTypes.STRING,
-  userId: {
-    type: DataTypes.INTEGER,
-    references: { model: 'users', key: 'id' }
-  },
-  timestamp: {
-    type: DataTypes.DATE,
-    defaultValue: DataTypes.NOW
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class searchHistory extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
   }
-});
-
-module.exports = searchHistory;
+  searchHistory.init({
+    query: DataTypes.STRING,
+    userId: {
+      type: DataTypes.INTEGER,
+      references: { model: 'users', key: 'id' }
+    }
+  }, {
+    sequelize,
+    modelName: 'searchHistory',
+  });
+  return searchHistory;
+};
