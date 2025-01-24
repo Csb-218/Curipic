c9b318f43b210e397e79e9edd9ed2da00e7e5ebb
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2025-01-25 00:32:10 +0530

    Create 20250123162749-create-photo.js

---
+++ migrations/20250123162749-create-photo.js
@@ -0,0 +1,42 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('photos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      imageUrl: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      altDescription: {
+        type: Sequelize.STRING
+      },
+      dateSaved: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        references: { model: 'users', key: 'id' }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('photos');
+  }
+};
