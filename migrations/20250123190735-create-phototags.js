70a577e986909253e2fb6c968053be45098e0056
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2025-01-25 00:32:18 +0530

    Create 20250123190735-create-phototags.js

---
+++ migrations/20250123190735-create-phototags.js
@@ -0,0 +1,41 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('phototags', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      tag_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'tags',
+          key: 'id'
+        },
+        onDelete: 'CASCADE'
+      },
+      photo_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'photos',
+          key: 'id'
+        },
+        onDelete: 'CASCADE'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('phototags');
+  }
+};
