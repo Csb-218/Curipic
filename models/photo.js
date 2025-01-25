94316eb371b211b59dc124a4f37a581d4136b5df
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2025-01-25 20:29:53 +0530

    Update photo.js

--- models/photo.js
+++ models/photo.js
@@ -1,21 +1,14 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
+const { Model } = require('sequelize');
+
+module.exports = (sequelize,DataTypes) => {
   class photo extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
-      photo.belongsToMany(models.tag,{
-        through:"phototags",
-        as:"tags",
-        foreignKey:"tag_id",
-      })
+      photo.belongsToMany(models.tag, {
+        through: 'phototags',
+        as: 'tags',
+        foreignKey: 'photo_id',
+      });
     }
   }
   photo.init({
